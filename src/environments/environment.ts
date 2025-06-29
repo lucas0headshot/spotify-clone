f5cc1eaa2705a9ecd9c392c2745240cfdb49bd38
lucas0headshot <lucas0headshot@gmail.com>
2025-06-28 21:28:12 -0300

    raw: upd environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  spotifyUrl: "https://api.spotify.com/v1/",
+  spotifyToken: "" // Pegue em: https://developer.spotify.com/
 };
 
 /*
